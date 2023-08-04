f40a9d4b48eaeb3899b594451ec0dfcd8fa20d03
jolonmusk <141039006+jolonmusk@users.noreply.github.com>
2023-08-04 12:42:38 +0900

    Add files via upload

---
+++ 최대 최소를 찾는 훌룡한 방법.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int n, num, min = 1000001, max = -1000001;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf_s("%d", &num);
+		if (num > max)
+			max = num;
+		if (num < min)
+			min = num;
+	}
+	printf("%d %d", min, max);
+
+	return 0;
+}
