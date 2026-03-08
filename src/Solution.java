ebf915e2944ce29d80d85a09f3e127f8613d81d1
jolonmusk <wanhui2398@naver.com>
2026-03-08 09:41:41 +0900

    Programmers, repeating characters

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,9 @@
-import java.util.ArrayList;
-
-public class Solution {
-    public int[] solution(int[] num_list){
-        ArrayList<Integer> array = new ArrayList<>();
-
-
-        for (int i : num_list) {
-            array.add(i);
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            sb.append((c + "").repeat(n));
         }
-
-        int last = num_list[num_list.length - 1];
-        int plast = num_list[num_list.length - 2];
-
-        if(last > plast) array.add(last - plast);
-        else array.add(last * 2);
-
-        int[] answer = array.stream().mapToInt(Integer::intValue).toArray();
-
-        return answer;
+        return sb.toString();
     }
-
-
 }
